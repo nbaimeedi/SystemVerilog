b87567a46cb8d0072abce0b592f48a773e4dfc5e
nbaimeedi <nbaimeedi@ucsd.edu>
2025-08-13 08:16:15 +0530

    "Verification Bug Fixed" --> Sampling in the next cycle after data is made available
    Data is put on mosi pin in the next cycle after chip select goes low. If we sample it at the moment data is put on mosi, we will get the previous values on mosi, thus we wait for the next positive edge of the sclk and then sample.

--- SPI/Verification/monitor.sv
+++ SPI/Verification/monitor.sv
@@ -22,8 +22,9 @@ class monitor;
       wait(sif.cs == 1'b0); //wait for the start of the transaction
       @(posedge sif.sclk); //wait for another positive edge of sclk because the data in put serially from the next clock after chip select goes low
       for (int i = 0; i <= 11; i++) begin //capture 12-bit data from 12 sclk cycles
+        @(posedge sif.sclk); // "Verification BUG" --> the data is put on mosi next cycle after the chip select goes to low so to sample it correctly, we wait for another positive edge of the sclk and then sample.
         serial_data[i] = sif.mosi; //capture the data on mosi and store it bit wise in the serial_data register
-        @(posedge sif.sclk);
+        //@(posedge sif.sclk); previous revision
       end
       wait(sif.cs == 1'b1); //wait for the end of transaction
       $display("[MON]: serial_data: %0h at Time: %0t", serial_data, $time);  
